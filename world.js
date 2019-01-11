be775f9ea0d82a947a24575be77b36a32b5ee974
velolisper <5976448+velolisper@users.noreply.github.com>
2019-01-11 14:08:31 -0500

    Added more to the list of creatures to add

--- world.js
+++ world.js
@@ -1,1 +1,9 @@
 // WORLD.JS
+
+function populateWorld() {
+
+}
+
+const originalCreatures = ['Dodo', 'Raptor', 'Triceratops', 'Parasaur', 'Meganeura', 'Dilophosaur', 'Carnotaurus', 'Stegosaur', 'Brontosaur', 'Paraceratherium', 'Trilobyte', 'Myrmidons', 'Rex', 'Allosaurus', 'Ankylosaur', 'Microraptor', 'Troodon', 'Compsagnathus', 'Dimorphodon', 'Dimetrodon', 'Spinosaurus', 'Carbonemys', 'Phiomia', 'Titanoboa', 'Beelzebufo', 'Achinata', 'Bear', 'Equus', 'Beaver', 'Pteranodon', 'Tapejara', 'Quetzelcoatlus', 'Argentavis', 'Pelagornis', 'Hesperornis', 'Icthyornis', 'Archeoptyrex', 'Mammoth', 'WRhino', 'Sabertooth', 'Baryonyx', 'Bat', 'Spider', 'Centipede', 'Mesopithecus', 'Gigantopithecus', 'Iguanadon', 'Kentrosaurus', 'Diplodocus', 'Glyptodon', 'Chalcotherium', 'Megatherium', 'Ichtyosaur', 'Plesiosaur', 'Coelecanth', 'Salmon', 'Piranha', 'Cnideria', 'Eel', 'Mosasaur', 'Eurypterid', 'GSquid', 'Lanturnfish', 'Basilosaurus', 'Megalosaurus', 'Elk', 'Giganotosaurus', 'Titanosaurus', 'Thylacoleo', 'Direwolf', 'Hyenadont', 'Enteledont', 'Leedsitchys', 'Bees', 'Leeches', 'Megalodon', 'Otter', 'Penguin', 'Phorusraccus', 'Sarcosuchus', 'Kaprosuchus', 'Therizinosaur']
+
+const newCreatures = ['Rorqual', 'Cachalot', 'Macrauchenia', 'Capybara', 'Bison']
