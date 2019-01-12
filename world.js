b1acaa5d1f9d82dbd478da072032737b8045a139
velolisper <5976448+velolisper@users.noreply.github.com>
2019-01-11 20:58:33 -0500

    Added more sections and refined model

--- world.js
+++ world.js
@@ -20,7 +20,14 @@ function activeWorld() {
 
 }
 
-var Animal = Object.create(null);
+var animal = {
+    // defining animal traits
+    create: function () {
+        let animal = Object.create(this);
+        return animal;
+    }
+}
+
 
 const originalCreatures = ['Dodo', 'Raptor', 'Triceratops', 'Parasaur', 'Meganeura', 'Dilophosaur', 'Carnotaurus', 'Stegosaur', 'Brontosaur', 'Paraceratherium', 'Trilobyte', 'Myrmidons', 'Rex', 'Allosaurus', 'Ankylosaur', 'Microraptor', 'Troodon', 'Compsagnathus', 'Dimorphodon', 'Dimetrodon', 'Spinosaurus', 'Carbonemys', 'Phiomia', 'Titanoboa', 'Beelzebufo', 'Achinata', 'Bear', 'Equus', 'Beaver', 'Pteranodon', 'Tapejara', 'Quetzelcoatlus', 'Argentavis', 'Pelagornis', 'Hesperornis', 'Icthyornis', 'Archeoptyrex', 'Mammoth', 'WRhino', 'Sabertooth', 'Baryonyx', 'Bat', 'Spider', 'Centipede', 'Mesopithecus', 'Gigantopithecus', 'Iguanadon', 'Kentrosaurus', 'Diplodocus', 'Glyptodon', 'Chalcotherium', 'Megatherium', 'Ichtyosaur', 'Plesiosaur', 'Coelecanth', 'Salmon', 'Piranha', 'Cnideria', 'Eel', 'Mosasaur', 'Eurypterid', 'GSquid', 'Lanturnfish', 'Basilosaurus', 'Megalosaurus', 'Elk', 'Giganotosaurus', 'Titanosaurus', 'Thylacoleo', 'Direwolf', 'Hyenadont', 'Enteledont', 'Leedsitchys', 'Bees', 'Leeches', 'Megalodon', 'Otter', 'Penguin', 'Phorusraccus', 'Sarcosuchus', 'Kaprosuchus', 'Therizinosaur']
 
