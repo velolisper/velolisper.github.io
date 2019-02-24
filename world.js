8b76bff22c69eb35d7f886d2055be801fdc90127
velolisper <5976448+velolisper@users.noreply.github.com>
2019-02-24 09:52:56 -0500

    update for feb

--- world.js
+++ world.js
@@ -17,7 +17,6 @@ function activeWorld() {
         // game loop here
 
     }
-
 }
 
 var animal = {
@@ -29,4 +28,6 @@ var animal = {
 }
 
 
-const originalCreatures = ['Dodo', 'Raptor', 'Triceratops', 'TimberRattler', 'Parasaur', 'Meganeura', 'Dilophosaur', 'Carnotaurus', 'Stegosaur', 'Brontosaur', 'Paraceratherium', 'Trilobyte', 'Myrmidons', 'Rex', 'Allosaurus', 'Ankylosaur', 'Microraptor', 'Troodon', 'Compsagnathus', 'Dimorphodon', 'Dimetrodon', 'Spinosaurus', 'Carbonemys', 'Deinotherium', 'Anaconda', 'Prionosuchus', 'Achinata', 'Bear', 'Equus', 'Beaver', 'Pteranodon', 'Tapejara', 'Quetzelcoatlus', 'Argentavis', 'Pelagornis', 'Hesperornis', 'Icthyornis', 'Archeoptyrex', 'Mammoth', 'WRhino', 'Sabertooth', 'Baryonyx', 'Bat', 'Spider', 'Centipede', 'Mesopithecus', 'Paranthropus', 'Iguanadon', 'Kentrosaurus', 'Diplodocus', 'Glyptodon', 'Chalcotherium', 'Megatherium', 'Ichtyosaur', 'Plesiosaur', 'Coelecanth', 'Salmon', 'Piranha', 'Cnideria', 'Eel', 'Mosasaur', 'Eurypterid', 'GSquid', 'Lanturnfish', 'Basilosaurus', 'Megalosaurus', 'Elk', 'Giganotosaurus', 'Titanosaurus', 'Thylacoleo', 'Direwolf', 'Hyenadont', 'Enteledont', 'Bees', 'Leeches', 'Megalodon', 'Otter', 'Penguin', 'Phorusraccus', 'Sarcosuchus', 'SeaKrait', 'Andrewsarchus', 'Therizinosaur', 'Rorqual', 'Cachalot', 'Macrauchenia', 'Capybara', 'Bison', 'Orca']
+const originalBeasts = ['Dodo', 'Raptor', 'Triceratops', 'TimberRattler', 'Parasaur', 'Meganeura', 'Dilophosaur', 'Carnotaurus', 'Stegosaur', 'Brontosaur', 'Paraceratherium', 'Trilobyte', 'Myrmidons', 'Rex', 'Allosaurus', 'Ankylosaur', 'Microraptor', 'Troodon', 'Compsagnathus', 'Dimorphodon', 'Dimetrodon', 'Spinosaurus', 'Carbonemys', 'Deinotherium', 'Anaconda', 'Prionosuchus', 'Achinata', 'Bear', 'Equus', 'Beaver', 'Pteranodon', 'Tapejara', 'Quetzelcoatlus', 'Argentavis', 'Pelagornis', 'Hesperornis', 'Icthyornis', 'Archeoptyrex', 'Mammoth', 'WRhino', 'Sabertooth', 'Baryonyx', 'Bat', 'Spider', 'Centipede', 'Mesopithecus', 'Paranthropus', 'Iguanadon', 'Kentrosaurus', 'Diplodocus', 'Glyptodon', 'Chalcotherium', 'Megatherium', 'Ichtyosaur', 'Plesiosaur', 'Coelecanth', 'Salmon', 'Piranha', 'Cnideria', 'Eel', 'Mosasaur', 'Eurypterid', 'GSquid', 'Lanturnfish', 'Basilosaurus', 'Megalosaurus', 'Elk', 'Giganotosaurus', 'Titanosaurus', 'Thylacoleo', 'Direwolf', 'Hyenadont', 'Enteledont', 'Bees', 'Leeches', 'Megalodon', 'Otter', 'Penguin', 'Phorusraccus', 'Sarcosuchus', 'SeaKrait', 'Andrewsarchus', 'Therizinosaur', 'Rorqual', 'Cachalot', 'Macrauchenia', 'Capybara', 'Bison', 'Orca', 'Yutyrannus']
+
+const originalPlants = ['Kelp', 'Teosinte', 'Pine', 'Cattail', 'Maple', 'Apple', 'Fern', 'Seagrass', 'Grape', 'Ivy', 'Sumac', 'Yew', 'Oak', 'Hemp', 'Barley', 'Rice', 'Nightshade', 'Rosemary', 'Deathcap', 'Skyshroom', 'Steakshroom', 'Lavender', 'Pitcher', 'GreatPitcher']
